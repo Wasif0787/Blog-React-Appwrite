5e85abe01832f7bfd026d760db371ce70991cf12
Wasif0787 <wasifhussain787@gmail.com>
2024-03-09 18:40:03 +0530

    Added Input Button components

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,13 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import LogoutBtn from "./LogoutBtn";
+import Logo from "./Logo";
+import Container from "./container/Container.jsx"
 
 export {
     Header,
-    Footer
+    Footer,
+    LogoutBtn,
+    Logo,
+    Container
 }
