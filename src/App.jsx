6d48801cc558b6d3944ae1816c184c54ae81787e
Wasif0787 <wasifhussain787@gmail.com>
2024-03-09 14:16:12 +0530

    Initialised redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,42 @@
-import './App.css'
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import { login, logout } from "./store/authSlice"
+import { Footer, Header } from './components'
+import "./App.css"
 
 function App() {
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }))
+        } else {
+          dispatch(logout())
+        }
+      })
+      .finally(() => setLoading(false))
+  }, [])
+
+  if (loading) {
+    return <div className="flex justify-center items-center h-screen text-5xl">
+      <h1>Loading...</h1>
+    </div>
+  }
+
   return (
-    <>
-      <h1>A Blog APP</h1>
-    </>
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          To-DO
+          {/* <Outlet/>*/}
+        </main>
+        <Footer />
+      </div>
+    </div>
   )
 }
 
