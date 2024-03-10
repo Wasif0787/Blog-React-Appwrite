92054ec35344714e87a1a330c9c0217d5fd205ec
Wasif0787 <wasifhussain787@gmail.com>
2024-03-10 18:52:01 +0530

    Added Postcard

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,13 @@ import Footer from "./Footer/Footer";
 import LogoutBtn from "./LogoutBtn";
 import Logo from "./Logo";
 import Container from "./container/Container.jsx"
+import Select from "./Select.jsx";
 
 export {
     Header,
     Footer,
     LogoutBtn,
     Logo,
-    Container
+    Container,
+    Select,
 }
